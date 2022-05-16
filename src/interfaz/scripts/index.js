7a6421fc2e91cc18759b758536697a4dbfc38cf8
jabr7 <65100012+jabr7@users.noreply.github.com>
2022-05-16 15:37:19 -0300

    Login boton, registro y movimiento del dominio original

--- src/interfaz/scripts/index.js
+++ src/interfaz/scripts/index.js
@@ -1,4 +1,6 @@
 import {MDCTextField} from '@material/textfield';
+import {MDCRipple} from '@material/ripple';
 
 const textField = new MDCTextField(document.getElementById('user'));
 const textField2 = new MDCTextField(document.getElementById('password'));
+const buttonRipple = new MDCRipple(document.getElementById('login_button'));
