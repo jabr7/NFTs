d2ed0c7d595914335813aa50e692f47b50180c41
Joaquin Bonifacino <joaquinbonifacino7@gmail.com>
2022-05-18 20:59:55 -0300

    Idea de como intercambiar paginas

--- src/interfaz/scripts/index.js
+++ src/interfaz/scripts/index.js
@@ -12,18 +12,22 @@ const login = new MDCRipple(document.getElementById('login_button'));
 const topAppBarElement = document.querySelector('.mdc-top-app-bar');
 const marketplace = new MDCRipple(document.getElementById('marketplace_button'));
 
+//Elimina el contenido de la pagina
 login.listen('click', () => {
-    
     if(user.value==="Joaquin" && password.value === "1234"){
-        
-        document.querySelectorAll(".login").forEach((element, index) => {
+        document.querySelectorAll(".content").forEach((element, index) => {
             element.classList.add("sample-content--hidden");
           });
-          
-         
     }else{
         alert("Error");
     }
+})
+
+//Trae de vuelta el login
+marketplace.listen('click', () => {
+    document.querySelectorAll(".login").forEach((element, index) => {
+         element.classList.remove("sample-content--hidden");
+    });
 })
 
 
