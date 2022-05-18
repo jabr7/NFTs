5baf5c94c1e189929af0c91fe167647e9845fada
jabr7 <65100012+jabr7@users.noreply.github.com>
2022-05-18 09:02:27 -0300

    documentacion

--- src/interfaz/scripts/index.js
+++ src/interfaz/scripts/index.js
@@ -10,7 +10,6 @@ const login = new MDCRipple(document.getElementById('login_button'));
 
 //Top Bar
 const topAppBarElement = document.querySelector('.mdc-top-app-bar');
-const topAppBar = new MDCTopAppBar(topAppBarElement);
 const marketplace = new MDCRipple(document.getElementById('marketplace_button'));
 
 
