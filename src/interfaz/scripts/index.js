c793683d11eb1253c72cb23f0caa711d13100124
jabr7 <65100012+jabr7@users.noreply.github.com>
2022-05-18 08:29:55 -0300

    Agregar top_bar

--- src/interfaz/scripts/index.js
+++ src/interfaz/scripts/index.js
@@ -1,9 +1,18 @@
 import {MDCTextField} from '@material/textfield';
 import {MDCRipple} from '@material/ripple';
 
+
+//Login Box
 const user = new MDCTextField(document.getElementById('user'));
 const password = new MDCTextField(document.getElementById('password'));
 const login = new MDCRipple(document.getElementById('login_button'));
+
+
+//Top Bar
+const topAppBarElement = document.querySelector('.mdc-top-app-bar');
+const topAppBar = new MDCTopAppBar(topAppBarElement);
+const marketplace = new MDCRipple(document.getElementById('marketplace_button'));
+
 
 
 login.listen('click', () => {
