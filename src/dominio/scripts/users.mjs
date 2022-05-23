30ed0164c48d93bae5ed5155c511dcbd45754933
tcaetano1995 <tomascaetano1995@gmail.com>
2022-05-23 13:11:23 -0300

    se agrega nombre a las cartas  (creado aleatoriamente)

---
+++ src/dominio/scripts/users.mjs
@@ -0,0 +1,8 @@
+let usuarios_login = [
+            {nombre:"admin",password:"admin.01",saldo:9999999,admin:true},
+            {nombre:"usuario1",password:"inicio.1",saldo:5000,admin:false},
+            {nombre:"usuario2",password:"inicio.2",saldo:100,admin:false},
+            {nombre:"usuario3",password:"inicio.3",saldo:0,admin:false}
+            ];
+
+export default usuarios_login;
