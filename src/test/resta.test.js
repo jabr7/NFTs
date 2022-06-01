ae6acbea02a9c6e54127d3516c47332142030587
tcaetano1995 <tomascaetano1995@gmail.com>
2022-06-01 11:19:10 -0300

    add coverage and group tests into describe

--- src/test/resta.test.js
+++ src/test/resta.test.js
@@ -1,9 +1,14 @@
+
 const resta = require('./resta');
 
-test('resta 1 - 2 to equal -1', () => {
-  expect(resta(1, 2)).toBe(-1);
-});
 
-test('adds 5 - 2 to equal 3', () => {
-    expect(resta(5, 1)).toBe(3);
+describe("Pruebas resta", () => {
+  test('resta 1 - 2 to equal -1', () => {
+    expect(resta(1, 2)).toBe(-1);
   });
+  
+  test('adds 5 - 2 to equal 3', () => {
+      expect(resta(5, 2)).toBe(3);
+    });
+
+});
--- src/test/sum.test.js
+++ src/test/sum.test.js
@@ -1,9 +1,16 @@
 const sum = require('./sum');
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+
+describe("Pruebas resta", () => {
+
+  test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3);
+  });
+  
+  test('adds 5 + 2 to equal 7', () => {
+      expect(sum(5, 2)).toBe(7);
+    });
+
+
 });
 
-test('adds 5 + 2 to equal 7', () => {
-    expect(sum(5, 2)).toBe(7);
-  });
