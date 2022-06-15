775dc1906abcd78e486be8f37138e33613b17a8b
jabr7 <joaquinbonifacino7@gmail.com>
2022-06-15 11:51:16 -0300

    documentacion user

--- src/dominio/users.mjs
+++ src/dominio/users.mjs
@@ -1,9 +1,10 @@
-let usuarios_login = [
-            {nombre:"admin",password:"admin",saldo:9999999,admin:true},
-            {nombre:"usuario1",password:"inicio.1",saldo:5000,admin:false},
-            {nombre:"usuario2",password:"inicio.2",saldo:100,admin:false},
-            {nombre:"usuario3",password:"inicio.3",saldo:0,admin:false},
-            {nombre:"dummy",password:"dummy",saldo:0,admin:false}
-            ];
+/* eslint-disable linebreak-style */
+const usuariosLogin = [
+  {nombre: 'admin', password: 'admin', saldo: 9999999, admin: true},
+  {nombre: 'usuario1', password: 'inicio.1', saldo: 5000, admin: false},
+  {nombre: 'usuario2', password: 'inicio.2', saldo: 100, admin: false},
+  {nombre: 'usuario3', password: 'inicio.3', saldo: 0, admin: false},
+  {nombre: 'dummy', password: 'dummy', saldo: 0, admin: false},
+];
 
-export default usuarios_login;
+export default usuariosLogin;
